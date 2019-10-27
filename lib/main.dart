63e63029e3b70ddb67021039b78f9a44c251b90e
Richard <richardbonneau91@gmail.com>
2019-10-26 23:06:58 -0400

    about to implement stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,36 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int leftDieNumber = 4;
+    int rightDieNumber = 1;
+    
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded( child: FlatButton(
+              child: Image.asset("images/dice$leftDieNumber.png"),
+              onPressed:(){print("left btn");}),
+          ),
+          Expanded(child: FlatButton(
+              child: Image.asset("images/dice$rightDieNumber.png"),
+              onPressed: (){print("right btn");},),
+          ),
+
+        ],
+      ),
+    );
   }
 }
+
+//class DicePage extends StatefulWidget {
+//  @override
+//  _DicePageState createState() => _DicePageState();
+//}
+//
+//class _DicePageState extends State<DicePage> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container();
+//  }
+//}
+
