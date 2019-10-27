402562c9ea6f554f94d0861ff2888a7b27a6f910
richardbonneau <richardbonneau@hotmail.com>
2019-10-27 14:24:39 -0400

    dice app completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,39 +16,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 4;
+  int rightDieNumber = 1;
   @override
   Widget build(BuildContext context) {
-    int leftDieNumber = 4;
-    int rightDieNumber = 1;
-    
     return Center(
       child: Row(
         children: <Widget>[
-          Expanded( child: FlatButton(
-              child: Image.asset("images/dice$leftDieNumber.png"),
-              onPressed:(){print("left btn");}),
+          Expanded(
+            child: FlatButton(
+                child: Image.asset("images/dice$leftDieNumber.png"),
+                onPressed: () {
+                  setState(() {
+                    rightDieNumber = Random().nextInt(6) + 1;
+                    leftDieNumber = Random().nextInt(6) + 1;
+                  });
+                  print("left btn");
+                }),
           ),
-          Expanded(child: FlatButton(
+          Expanded(
+            child: FlatButton(
               child: Image.asset("images/dice$rightDieNumber.png"),
-              onPressed: (){print("right btn");},),
+              onPressed: () {
+                setState(() {
+                  rightDieNumber = Random().nextInt(6) + 1;
+                  leftDieNumber = Random().nextInt(6) + 1;
+                });
+                print("right btn");
+              },
+            ),
           ),
-
         ],
       ),
     );
   }
 }
-
-//class DicePage extends StatefulWidget {
-//  @override
-//  _DicePageState createState() => _DicePageState();
-//}
-//
-//class _DicePageState extends State<DicePage> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Container();
-//  }
-//}
-
